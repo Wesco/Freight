691a1fcba1ac53993dd280cb6b05f096dc088d91
TReische <treische@wesco.com>
2014-04-30 11:11:14 -0400

    Added config class

---
+++ config.py
@@ -0,0 +1,37 @@
+'''
+Created on Apr 30, 2014
+
+@author: TReische
+'''
+
+from ConfigParser import ConfigParser
+
+
+class config(object):
+    def __init__(self):
+
+        # Load configuration file
+        self._cfg_defaults = {'watch-dir': '/home/pi/shared/ups',
+                              'poi-dir': '/home/pi/shared/POI History',
+                              'oor-dir': '/home/pi/shared/117 History'}
+
+        self._cfg = ConfigParser(self._cfg_defaults)
+        self._cfg.read('config.ini')
+
+        if not self._cfg.has_section('settings'):
+            self._cfg.add_section('settings')
+
+    def reload_config(self):
+        self._cfg.read('config.ini')
+
+    @property
+    def watch_dir(self):
+        return self._cfg.get('settings', 'watch-dir')
+
+    @property
+    def poi_dir(self):
+        return self._cfg.get('settings', 'poi-dir')
+
+    @property
+    def oor_dir(self):
+        return self._cfg.get('settings', 'oor-dir')
