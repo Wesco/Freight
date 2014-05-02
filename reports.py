435f29f5b940482e99111f617085c0f311b98a56
TReische <treische@wesco.com>
2014-05-02 11:09:50 -0400

    moved imports into functions

--- reports.py
+++ reports.py
@@ -4,16 +4,12 @@ Created on Apr 30, 2014
 @author: TReische
 '''
 
-from pandas import merge
-from datetime import datetime, timedelta
-from os import listdir
-from fnmatch import fnmatch
-
 
 def Poi(poi_dir):
     name_func = [lambda x: 'POI HISTORY ' + x + '.csv',
                  lambda x: 'POI OPEN ' + x + '.csv']
     file_list = _get_file_list(poi_dir, name_func)
+
     return _merge_data(file_list, len(file_list))
 
 
@@ -23,7 +19,11 @@ def Oor(oor_dir):
     return _merge_data(file_list, len(file_list))
 
 
-def _get_file_list(poi_dir, name_func):
+def _get_file_list(file_dir, name_func):
+    from fnmatch import fnmatch
+    from datetime import datetime, timedelta
+    from os import listdir
+
     poilst = []
     for i in range(0, 300):
         dt = (datetime.now() - timedelta(days=i)).strftime('%Y-%m-%d')
@@ -34,12 +34,14 @@ def _get_file_list(poi_dir, name_func):
         else:
             poilst.append(name_func(dt))
 
-    poifiles = [x for x in listdir(poi_dir) if fnmatch(x, '*.csv')]
+    poifiles = [x for x in listdir(file_dir) if fnmatch(x, '*.csv')]
     poilst = [x for x in poifiles if x in poilst]
     return poilst
 
 
 def _merge_data(lst, length):
+    from pandas import merge
+
     l = length - 1
 
     if l == 1:
