c5f1728151e640af3f8a3d14d532f9f86c5cbdfe
TReische <treische@wesco.com>
2014-05-15 16:35:25 -0400

    Cleaned up formatting

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
-import py2exe, sys, os
-
+import py2exe  # @UnusedImport
+import sys
 
 sys.argv.append('py2exe')
 
 setup(
-      options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-      windows = [{'script': "freight.py"}],
-      zipfile = None,
+      options={'py2exe': {'bundle_files': 1, 'compressed': True}},
+      windows=[{'script': "freight.py"}],
+      zipfile=None,
       )
