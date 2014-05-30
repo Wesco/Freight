2768168822cb849543187ddd40a6f55c55bd3fdd
TReische <treische@wesco.com>
2014-05-30 10:48:17 -0400

    Added items to panel4

--- freightui.py
+++ freightui.py
@@ -24,6 +24,8 @@ class MainWindow(wx.Frame):
         wx.Frame.__init__(self, None, -1, title)
         self.listbox = None
         self.listbox2 = None
+        self.upstextctrl = None
+        self.outtextctrl = None
         self.OnInit()
         self.Layout()
         self.Show()
@@ -35,6 +37,8 @@ class MainWindow(wx.Frame):
         panel2.SetSizer(wx.BoxSizer(wx.HORIZONTAL))
         panel3 = wx.Panel(self, -1)
         panel3.SetSizer(wx.FlexGridSizer(rows=2, cols=3, vgap=5, hgap=5))
+        panel4 = wx.Panel(self, -1)
+        panel4.SetSizer(wx.BoxSizer(wx.HORIZONTAL))
 
         hsizer = wx.BoxSizer(wx.HORIZONTAL)
         hsizer.Add(panel1, 0, wx.EXPAND)
@@ -43,6 +47,7 @@ class MainWindow(wx.Frame):
         vsizer = wx.BoxSizer(wx.VERTICAL)
         vsizer.Add(hsizer, 1, wx.EXPAND)
         vsizer.Add(panel3, 0, wx.EXPAND)
+        vsizer.Add(panel4, 0, wx.EXPAND)
 
         # Add to panel1
         self.listbox = wx.ListCtrl(parent=panel1,
@@ -83,21 +88,31 @@ class MainWindow(wx.Frame):
 
         # Panel 3
         upsstatictext = wx.StaticText(panel3, id=201, label='UPS Report')
-        upstextctrl = wx.TextCtrl(panel3, id=202)
+        self.upstextctrl = wx.TextCtrl(panel3, id=202)
         upsbutton = wx.Button(panel3, id=203, label='Browse')
+        self.Bind(wx.EVT_BUTTON, self.OnUpsButton, id=203)
 
         outputstatictext = wx.StaticText(panel3, id=301, label='Output File')
-        outputtextctrl = wx.TextCtrl(panel3, id=302)
-        outputbutton = wx.Button(panel3, id=303, label='Browse')
+        self.outtextctrl = wx.TextCtrl(panel3, id=302)
+        outbutton = wx.Button(panel3, id=303, label='Browse')
+        self.Bind(wx.EVT_BUTTON, self.OnOutButton, id=303)
 
         panel3.Sizer.AddMany([(upsstatictext, 0, wx.LEFT, 7),
-                              (upstextctrl, 0, wx.EXPAND),
+                              (self.upstextctrl, 0, wx.EXPAND),
                               (upsbutton, wx.RIGHT, 7),
                               (outputstatictext, 0, wx.LEFT, 7),
-                              (outputtextctrl, 0, wx.EXPAND | wx.BOTTOM, 7),
-                              (outputbutton, 0, wx.BOTTOM | wx.RIGHT, 7)])
+                              (self.outtextctrl, 0, wx.EXPAND | wx.BOTTOM),
+                              (outbutton, 0, wx.BOTTOM | wx.RIGHT)])
 
         panel3.Sizer.AddGrowableCol(1, 0)
+
+        # Panel 4
+        processbtn = wx.Button(panel4, id=401, label='Process Report')
+        panel4.Sizer.Add(item=processbtn,
+                         proportion=1,
+                         flag=wx.ALL,
+                         border=5)
+        self.Bind(wx.EVT_BUTTON, self.ProcessReport, id=401)
 
         self.SetAutoLayout(True)
         self.SetSizer(vsizer)
@@ -110,6 +125,21 @@ class MainWindow(wx.Frame):
         self.listbox2.DeleteAllItems()
         for y in lst:
             self.listbox2.Append(y)
+
+    def OnUpsButton(self, event):
+        dialog = wx.FileDialog(self, "Select a file", 'C:\\')
+        modal = dialog.ShowModal()
+        if modal == wx.ID_OK:
+            self.upstextctrl.SetValue(dialog.GetPath())
+
+    def OnOutButton(self, event):
+        dialog = wx.DirDialog(self, "Select a folder", 'C:\\')
+        modal = dialog.ShowModal()
+        if modal == wx.ID_OK:
+            self.outtextctrl.SetValue(dialog.GetPath())
+
+    def ProcessReport(self, event):
+        print 'Process Report!'
 
 
 class freightApp(wx.App):
