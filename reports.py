cb6250801ab3b81acf1a28e733741b9884fa738c
TReische <treische@wesco.com>
2015-02-16 16:28:06 -0500

    Reports rework
    Changed docstrings to double quote character
    Changed poi to accept a date
    Renamed Oor to customers
    Added date parameter to customers
    Renamed Gaps to suppliers
    Renamed SM to sales_margin
    Changed _get_file_list to accept a start date
    Changed _read_files usecols parameter name

--- reports.py
+++ reports.py
@@ -1,53 +1,56 @@
-'''
+"""
 Created on Apr 30, 2014
 
 @author: TReische
-'''
+"""
 
 import pandas as pd
 import numpy as np
+import re
 from datetime import datetime, timedelta
 from fnmatch import fnmatch
 from os import listdir, path
-from dateutil.relativedelta import relativedelta
 
 
-def Poi(open_poi_dir, history_poi_dir):
+def poi(open_poi_dir: str, history_poi_dir: str, ups_date = datetime.now()) -> pd.DataFrame:
     """
     Return a DataFrame containing POI columns ' PO NUMBER' and 'ORDER'
+    :rtype : pd.DataFrame
     """
 
     # Get list of POI files
     name_func = lambda x: 'POI HISTORY ' + x + '.csv'
-    file_list = _get_file_list(history_poi_dir, name_func)
+    file_list = _get_file_list(history_poi_dir, name_func, ups_date)
 
     name_func = lambda x: 'POI OPEN ' + x + '.csv'
-    file_list.extend(_get_file_list(open_poi_dir, name_func))
+    file_list.extend(_get_file_list(open_poi_dir, name_func, ups_date))
 
     # Read the files and merge them into a DataFrame
     df_list = _read_files(file_list,
                           header=1,
                           skip_footer=0,
-                          usecols=[' PO NUMBER', 'ORDER'],
-                          converters={0: str, 1: str})
+                          use_cols=[' PO NUMBER', 'ORDER', 'SUPPLIER'],
+                          converters={0: str, 1: str, 2: str})
     df = _merge_df(df_list, len(df_list), ' PO NUMBER')
+    df['SUPPLIER'] = df['SUPPLIER'].apply(str.upper)
     return df
 
 
-def Oor(oor_dir):
+def customers(oor_dir: str, branch: str, ups_date=datetime.now()) -> pd.DataFrame:
     """
-    Return a DataFrame containing OOR columns 'CUSTOMER', and 'ORDER NO'
+    Return a DataFrame containing CUSTOMERS indexed by ORDER NO
+    :rtype : pd.DataFrame
     """
 
     # Get list of OOR files
-    name_func = lambda x: '3615 ' + x + ' ALLORDERS.csv'
-    file_list = _get_file_list(oor_dir, name_func)
+    name_func = lambda x: branch + ' ' + x + ' ALLORDERS.csv'
+    file_list = _get_file_list(oor_dir, name_func, ups_date)
 
     # Read the files and merge them into a DataFrame
     df_list = _read_files(file_list,
                           header=1,
                           skip_footer=1,
-                          usecols=[2, 3],
+                          use_cols=[2, 3],
                           converters={'CUSTOMER': str, 'ORDER NO': str})
 
     df = _merge_df(df_list, len(df_list), 'ORDER NO')
@@ -56,84 +59,101 @@ def Oor(oor_dir):
     return df
 
 
-def Gaps(gaps_dir, branch):
+def suppliers(gaps_dir: str, branch: str) -> pd.DataFrame:
     """
-    Return a DataFrame containing Gaps
+    Return a DataFrame containing stock suppliers indexed by SIM numbers
+    :rtype : pd.DataFrame
     """
 
-    fileName = ""
-    gaps = None
+    file_loc = ""
+    file_found = False
 
     # Look back up to 180 days for a GAPS report
     for i in range(0, 180):
         dt = datetime.today() - timedelta(days=i)
-        fileName = "%s %s%s" % (branch, dt.strftime('%Y-%m-%d'), '.csv')
-        fileLoc = path.join(gaps_dir, fileName)
+        file_name = "%s %s%s" % (branch, dt.strftime('%Y-%m-%d'), '.csv')
+        file_path = path.join(gaps_dir, dt.strftime('%Y'))
+        file_loc = path.join(file_path, file_name)
 
-        if path.isfile(fileLoc):
-            gaps = pd.read_csv(fileLoc,
-                             encoding='cp1250',
-                             na_values=['  '])
+        if path.isfile(file_loc):
+            file_found = True
             break
 
-    if gaps is None:
-        raise FileNotFoundError("Gaps report not found")
+    if file_found is False:
+        raise FileNotFoundError(2, "Gaps report not found")
+
+    gaps = pd.read_csv(file_loc,
+                       encoding='cp1250',
+                       na_values=['  '],
+                       usecols=['Sim_mfr_no', 'Sim_item_no', 'Supplier_no'],
+                       dtype={'Sim_mfr_no': str,
+                              'Sim_item_no': str,
+                              'Supplier_no': str})
+
+    gaps.set_index(gaps['Sim_mfr_no'] + gaps['Sim_item_no'], inplace=True)
+    del [gaps['Sim_mfr_no'], gaps['Sim_item_no']]
 
     return gaps
 
 
-def SM(sm_dir, months=6):
+def sales_margin(sm_dir: str, months: int=6) -> pd.DataFrame:
     """
     Return a DataFrame containing Sales and Margin data
+    :rtype : pd.DataFrame
     """
 
-    # List of wanted files
-    sm_list = []
-    for dt in _prev_months(6):
-        fname = 'SM ' + dt.strftime('%Y-%m') + '-01.csv'
-        sm_list.append(fname)
+    # Create a list of Sales & Margin files
+    sm_list = [x for x in listdir(sm_dir)
+               if re.search(r'^SM \d{4}-\d{2}-\d{2}\.csv$', x)]
 
-    # List of existing files
-    sm_files = [x for x in listdir(sm_dir) if fnmatch(x, '*.csv')]
+    # Clamp months to the available Sales & Margin range
+    months = _clamp(months, 1, len(sm_list))
 
-    # Wanted files that exist
-    file_list = [path.join(sm_dir, x) for x in sm_files if x in sm_list]
+    # Sort so that the most recent months are first
+    sm_list.sort(reverse=True)
 
-    df_list = _read_files(file_list,
-                         header=0,
-                         skip_footer=0,
-                         usecols=['dpc', 'dpc_name', 'mfr', 'item', 'sales'],
-                         engine='c',
-                         dtype={'dpc': str,
-                                'dpc_name': str,
-                                'mfr': str,
-                                'item': str,
-                                'sales': float})
+    # Join the full path and file names
+    # shrink list to the correct number of months
+    sm_list = [path.join(sm_dir, x) for x in sm_list[:months]]
 
-    for i in range(0, len(df_list)):
-        df = df_list[i]
-        df_list[i] = df[df['sales'].apply(lambda y: y > 0)].dropna()
+    # Create a list of sales and margin DataFrames
+    df_list = []
+    for i in range(len(sm_list)):
+        df_list.append(pd.read_csv(sm_list[i], encoding='cp1250', na_values=[' ', '     ', ''], usecols=['dpc', 'dpc_name', 'mfr', 'item', 'cost'], dtype={'dpc': str, 'dpc_name': str, 'mfr': str, 'item': str, 'cost': np.float}))
 
-    merged_df = pd.concat(df_list)
-    return merged_df
+    sm_df = pd.concat(df_list)
+    sm_df['sim'] = sm_df['mfr'] + sm_df['item']
+    sm_df.dropna(inplace=True)
+    sm_df = sm_df[sm_df['cost'] > 0]
+    sm_df.reset_index(inplace=True, drop=True)
+
+    return sm_df
 
 
-def _get_file_list(file_dir, name_func):
+def _clamp(value, min_value, max_value):
+    """
+    Return a value within the specified range
+    """
+
+    return sorted((min_value, value, max_value))[1]
+
+
+def _get_file_list(file_dir: str, name_func, start_date=datetime.now()) -> list:
     """
     Return a list of files
     """
 
-    poilst = []
-    for i in range(0, 300):
-        dt = (datetime.now() - timedelta(days=i)).strftime('%Y-%m-%d')
-        poilst.append(name_func(dt))
+    poi_list = []
+    for i in range(0, 80):
+        dt = (start_date - timedelta(days=i)).strftime('%Y-%m-%d')
+        poi_list.append(name_func(dt))
 
-    poifiles = [x for x in listdir(file_dir) if fnmatch(x, '*.csv')]
-    poilst = [path.join(file_dir, x) for x in poifiles if x in poilst]
-    return poilst
+    poi_files = [x for x in listdir(file_dir) if fnmatch(x, '*.csv')]
+    poi_list = [path.join(file_dir, x) for x in poi_files if x in poi_list]
+    return poi_list
 
 
-def _read_files(file_list, header, skip_footer, usecols,
+def _read_files(file_list, header, skip_footer, use_cols,
                 engine='python', converters=None, dtype=None):
     """
     Return a list of DataFrames.
@@ -142,12 +162,12 @@ def _read_files(file_list, header, skip_footer, usecols,
     lst = []
     for _file in file_list:
         lst.append(pd.read_csv(_file,
-                            header=header,
-                            skip_footer=skip_footer,
-                            usecols=usecols,
-                            converters=converters,
-                            dtype=dtype,
-                            engine=engine))
+                               header=header,
+                               skip_footer=skip_footer,
+                               usecols=use_cols,
+                               converters=converters,
+                               dtype=dtype,
+                               engine=engine))
     return lst
 
 
@@ -165,7 +185,7 @@ def _merge_df(lst, length, drop_on=None):
                       how='outer',
                       sort=True)
 
-    if drop_on != None:
+    if drop_on is not None:
         df.drop_duplicates(drop_on, inplace=True)
 
     return df
