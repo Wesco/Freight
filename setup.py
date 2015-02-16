1c473db2265d1ef485c3782b18c95fbf801d231a
TReische <treische@wesco.com>
2015-02-16 16:17:57 -0500

    Made setup file more readable

--- setup.py
+++ setup.py
@@ -5,15 +5,21 @@ import sys
 sys.argv.append('py2exe')
 
 setup(
-      options={
-               'py2exe': {
-                          'includes': ['numpy.linalg._umath_linalg',
-                                       'numpy.fft.fftpack_lite'],
-                          'bundle_files': 1,
-                          'optimize': 2,
-                          'compressed': True
-                          }
-               },
-      windows=[{'script': "freight.py"}],
-      zipfile=None,
-      )
+    options=
+    {
+        'py2exe':
+        {
+            'includes':
+            [
+                'numpy.linalg._umath_linalg',
+                'numpy.fft.fftpack_lite'
+            ],
+            'bundle_files': 1,
+            'optimize': 2,
+            'compressed': True
+        }
+    },
+    windows=[{'script': "freight.py"}],
+    zipfile=None,
+    requires=['pandas', 'xlrd']
+)
