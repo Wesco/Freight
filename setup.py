c16d068fb023914e651018436d454211a2b3c2b7
TReische <treische@wesco.com>
2015-02-18 11:37:03 -0500

    Changed freeze module from py2exe to cx_freeze

--- setup.py
+++ setup.py
@@ -1,25 +1,19 @@
-from distutils.core import setup
-import py2exe  # @UnusedImport
 import sys
+from cx_Freeze import setup, Executable
 
-sys.argv.append('py2exe')
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"],
+                     "includes": ["pandas", "pytz", "win32timezone"],
+                     "excludes": ["tkinter", "PySide", "matplotlib", "scipy"]}
 
-setup(
-    options=
-    {
-        'py2exe':
-        {
-            'includes':
-            [
-                'numpy.linalg._umath_linalg',
-                'numpy.fft.fftpack_lite'
-            ],
-            'bundle_files': 1,
-            'optimize': 2,
-            'compressed': True
-        }
-    },
-    windows=[{'script': "freight.py"}],
-    zipfile=None,
-    requires=['pandas', 'xlrd']
-)
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(name="freight",
+      version="0.1",
+      description="Freight processor",
+      options={"build_exe": build_exe_options},
+      executables=[Executable("freight.py", base=base)])
